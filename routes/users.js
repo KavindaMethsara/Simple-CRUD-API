da32556008a0f757b48e38df5b1d89a7a984bcc4
KavindaMethsara <methsara31@gmail.com>
2023-11-11 00:09:25 +0530

    Create a ID for users

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,22 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "Peter",
-        lastName: "Parker",
-        age: 25
-    },
-    {
-        firstName: "Mary",
-        lastName: "Jane",
-        age: 23
-    }
-]
+const users = []
 
+//Route fro get users
 router.get('/', (req, res) => {
     console.log(users);
     res.send(users);
 });
 
+//Route fro add users
 router.post('/', (req, res) => {
     const user = req.body;
 
-    users.push(user);
+    users.push({ ...user, id: uuidv4()});
 
     res.send(`User with the name ${user.firstName} added to the database!`);
 });
